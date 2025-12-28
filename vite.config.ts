e0ba50cc46d9f1a515bf40bd13a65db81da66f28
Samuel Erwin <Samuel Erwin SREphotoMN@gmail.com>
2025-12-27 19:41:47 -0600

    Fix base path for Render

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { defineConfig, loadEnv } from 'vite';
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, '.', '');
   return {
-    base: "/Interactive-App-Deployment-Launchpad-/",
+    base: "/",
     server: {
       port: 3021,
       host: '0.0.0.0',
